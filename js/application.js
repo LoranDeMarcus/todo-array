15884e63a6f4c0259309517d32b37c4059640f0e
LoranDeMarcus <lorandemarcus@gmail.com>
2017-04-08 23:55:11 +0300

    Разобраться с удалением половины элементов

--- js/application.js
+++ js/application.js
@@ -1,6 +1,80 @@
 var tasks = [];
+var i = 0;
+// Разобраться с удалением половины элементов
+function updateTasks() {
+    $('#todo-list').find('li').remove();
+    $(tasks).each(function(i) {
+        $('#todo-list').append('<li class="completed">\
+            <div class="todo-task">\
+            <label class="text"><input class="toggle" type="checkbox">'+ tasks[i].title +'</label>\
+            <button class="destroy"></button>\
+            </div>\
+            </li>');
+    });
+    tasksCount();
+}
+
+function tasksCount() {
+    $('.count').text(tasks.length);
+    if (tasks.length < 1) {
+        $('#footer').hide();
+    }
+}
 
 /* Добавление задач */
+
+$('#new-todo').keyup(function (event) {
+    if ((event.keyCode == 13)) {
+        if ($(this).val() !== '') {
+            $('#main').show();
+            $('#footer').show();
+            var $this = $(this);
+            var newTask = $this.val();
+            tasks.push({id: i++, title: newTask, status: 'active'});
+            updateTasks();
+            // Очистка Input
+            $('#new-todo').val('');
+        } else {
+            return false;
+        }
+    }
+});
+
+/* Удаление */
+
+$('#todo-list').on('click', 'button.destroy', function () {
+    $.each(tasks, function (id) {
+        tasks.splice(id, 1);
+    });
+    updateTasks();
+    tasksCount();
+});
+
+
+function ShowTasks() {
+    tasks.forEach(function (item, i, tasks) {
+        console.log(tasks[i].id, tasks[i].title, tasks[i].status);
+    });
+}
+/*
+var tasks = [];
+var i = 0;
+
+function updateTasks() {
+    var $li = $(this).closest('li');
+    $li.remove();
+    tasks.forEach(function (title, id, tasks) {
+        tasks.splice(i, 1);
+    });
+    $('#todo-list').append('<li class="completed">\
+            <div class="todo-task">\
+            <label class="text"><input class="toggle" type="checkbox">'+ tasks[i].title +'</label>\
+            <button class="destroy"></button>\
+            </div>\
+            </li>');
+};
+
+/* Добавление задач
 
 function tasksCount () {
     var count = $('#todo-list').find('li');
@@ -8,7 +82,7 @@ function tasksCount () {
     if (count.length < 1) {
         $('#footer').hide();
     }
-}
+};
 
 $('#new-todo').keyup(function (event) {
     if ((event.keyCode == 13)) {
@@ -23,18 +97,17 @@ $('#new-todo').keyup(function (event) {
             </li>');
             var $this = $(this);
             var newTask = $this.val();
-            tasks.push(newTask);
+            tasks.push({id: i++, title: newTask, status: 'active'});
             tasksCount();
             // Очистка Input
             $('#new-todo').val('');
-        }
-        else {
+        } else {
             return false;
-        }
-    }
+        };
+    };
 });
 
-/* Checkbox */
+/* Checkbox
 
 $('#todo-list').on('click', '.toggle', function() {
     $(this).parents('li').toggleClass('checked');
@@ -48,7 +121,7 @@ $('#todo-list').on('click', '.toggle', function() {
     }
     if (!$('li.complete').hasClass('checked')) {
         $('#clear-completed').hide();
-    }
+    };
     tasksCount();
 });
 
@@ -65,16 +138,16 @@ $('#toggle-all').on('click', function () {
     }
     if (!$('li.complete').hasClass('checked')) {
         $('#clear-completed').hide();
-    }
+    };
     tasksCount();
 });
 
-/* События отображения задач */
+/* События отображения задач
 
 $('.show-all-tasks').on('click', function() {
     $('li.complete').each(function () {
         $(this).show();
-    })
+    });
     $(this).toggleClass('active');
     $('.show-active-tasks').removeClass('active');
     $('.show-completed-tasks').removeClass('active');
@@ -96,17 +169,17 @@ $('.show-completed-tasks').on('click', function() {
     $('.show-active-tasks').removeClass('active');
 });
 
-/* События кнопки Clear Completed */
+/* События кнопки Clear Completed
 
 $('#clear-completed').on('click', function () {
     $('input:checked').parents('li').remove();
     tasksCount();
 });
 
-/* Удаление */
+/* Удаление
 
 $('#todo-list').on('click', 'button.destroy', function () {
-    var $li = $(this).closest('li');
-    $li.remove();
+    updateTasks();
     tasksCount();
-});
+})
+*/
